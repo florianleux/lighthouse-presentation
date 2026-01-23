e018a9046e3b98464937dcddc69e86e92d6faef7
florianleux <10267793+florianleux@users.noreply.github.com>
2026-01-23 10:32:44 +0100

    fix env variable

--- apps/vote/vite.config.ts
+++ apps/vote/vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    'import.meta.env.VITE_ABLY_API_KEY': JSON.stringify(process.env.VITE_ABLY_API_KEY)
+  },
   server: {
     port: 3032,
     host: true,
